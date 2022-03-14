f35efba48effe89649c94d0056f4ab8aaae6bf8b
khadiza21 <bibikhadiza474@gmail.com>
2022-03-14 12:32:57 +0600

    1006 distance cpp

---
+++ beginnerUri/1006-distance.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int x, distance;
+    cin >> x;
+    distance = x * 2;
+    cout << distance << " minutos" << endl;
+
+    return 0;
+}
