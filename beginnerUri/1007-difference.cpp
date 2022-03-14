ad709761ef8a65d72369f0a2559bc675fcb42bd6
khadiza21 <bibikhadiza474@gmail.com>
2022-03-14 09:49:59 +0600

    1007 difference

---
+++ beginnerUri/1007-difference.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int A, B, C, D, difference;
+    cin >> A >> B >> C >> D;
+    difference = (A * B - C * D);
+    cout << "DIFERENCA = " << difference << endl;
+
+    return 0;
+}
