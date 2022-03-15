c2ee0c64a3ac2faa20ba8bc0e4fe097324e6c7ac
khadiza21 <bibikhadiza474@gmail.com>
2022-03-15 19:51:23 +0600

    1019 time conversion

--- beginnerUri/1019-timeConversion.cpp
+++ beginnerUri/1019-timeConversion.cpp
@@ -5,8 +5,26 @@ using namespace std;
 int main()
 {
 
-    int number, time, hours, minutes, seconds;
+    int number, hours, minutes, seconds;
     cin >> number;
+
+    if (number < 60)
+    {
+        cout << 0 << ":" << 0 << ":" << number << endl;
+    }
+    else if (number < 3600)
+    {
+        minutes = number / 60;
+        seconds = number % 60;
+        cout << 0 << ":" << minutes << ":" << seconds << endl;
+    }
+    else
+    {
+        hours = number / 3600;
+        minutes = (number % 3600) / 60;
+        seconds = (number % 3600) % 60;
+        cout << hours << ":" << minutes << ":" << seconds << endl;
+    }
 
     return 0;
 }
---
+++ beginnerUri/1042-simpleSort.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int array1[3];
+    for (int i = 1; i < 4; i++)
+    {
+        cin >> array1[i];
+    }
+    cout << endl;
+
+    // int indexCheck = array1[0];
+    // for (int i = 1; i < 4; i++)
+    // {
+    //     if (indexCheck < array1[i])
+
+    //     indexCheck = array1[i];
+    // }
+
+    cout << endl << endl;
+
+    // for (int i = 1; i < 4; i++)
+    // {
+    //     cout << array1[i] << endl;
+    // }
+    // 5cout<<array1;
+    return 0;
+}
